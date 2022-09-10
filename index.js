0fa48c1147ee60e2a47e4ee6131d1fafb5e2145f
marionsy <marionantoniette.sy@gmail.com>
2022-09-10 14:33:06 -0700

    added department choices and required console.table

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Require dependencies
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 // Connect to database
 const db = mysql.createConnection(
@@ -124,49 +125,59 @@ addDepartment = () => {
 
 // Function to add a role
 addRole = () => {
-    inquirer.prompt([
-        {
-            name: "name",
-            type: "input",
-            message: "What is the name of the role?",
-            validate: (value) => {
-                if (value) {
-                    return true;
-                } else {
-                    console.log("Please enter a role.");
+    db.query('SELECT * FROM department', (err, results) => {
+        if (err) throw err;
+        const departments = results;
+        const departmentNames = departments.map(department => department.name);
+
+        inquirer.prompt([
+            {
+                name: "role",
+                type: "input",
+                message: "What is the name of the role?",
+                validate: (value) => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log("Please enter a role.");
+                    }
+                }
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary for the role?",
+                validate: (value) => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log("Please enter a salary for the role.");
+                    }
+                }
+            },
+            {
+                name: "department",
+                type: "list",
+                message: "What is the department for the role?",
+                choices: departmentNames,
+                validate: (value) => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log("Please enter a department for the role.");
+                    }
                 }
             }
-        },
-        {
-            name: "salary",
-            type: "input",
-            message: "What is the salary for the role?",
-            validate: (value) => {
-                if (value) {
-                    return true;
-                } else {
-                    console.log("Please enter a salary for the role.");
-                }
-            }
-        },
-        {
-            name: "department",
-            type: "input",
-            message: "What is the department for the role?",
-            validate: (value) => {
-                if (value) {
-                    return true;
-                } else {
-                    console.log("Please enter a department for the role.");
-                }
-            }
-        }
-    ]).then(answer => {
-        const query = "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
-        db.query(query, [answer.name, parseInt(answer.salary), parseInt(answer.department)], (err, results) => {
-            if (err) throw err;
-            console.log("Successfully added new role.");
-            init();
+        ]).then(answer => {
+            const query = "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
+            const deparmentId = departments
+                .filter(department => department.name === answer.department)
+                .map(department => department.id);
+            db.query(query, [answer.role, parseInt(answer.salary), deparmentId], (err, results) => {
+                if (err) throw err;
+                console.log("Successfully added new role.");
+                init();
+            });
         });
     });
 }
